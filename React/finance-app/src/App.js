892453f6ac90d9ad998ade89260e3d49bbe23ac7
Mehek Bhatnagar <mehekb@Meheks-MacBook-Air.local>
2024-12-29 19:41:37 -0800

    Added a table to show all transactions so far. Fixed main.py in FastAPI to correctly add middleware

--- React/finance-app/src/App.js
+++ React/finance-app/src/App.js
@@ -95,6 +95,30 @@ const App = () => {
           </button>
 
         </form>
+
+        <table className='table table-striped table-bordered table-hover mt-3'>
+        <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Category</th>
+              <th>Description</th>
+              <th>Income?</th>
+              <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.is_income ? 'Yes' : 'No'}</td>
+              <td>{transaction.date}</td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+
       </div>
 
     </div>
